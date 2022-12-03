f27b6b767419cd0756321ebaecfda1404acdcf93
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-12-03 16:34:53 +0300

    Update day3

--- day-3/1.ts
+++ day-3/1.ts
@@ -1,33 +1,5 @@
 export {};
-// const expected = 157;
-// const text = await Deno.readTextFile("./input.txt");
-// const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// const sacks = text.split("\n").map((sack) => {
-//   const firstHalf = sack.slice(0, sack.length / 2);
-//   const secondHalf = sack.slice(sack.length / 2);
-
-//   let account = [];
-
-//   for (let i = 0; i < firstHalf.length; i++) {
-//     for (let j = 0; j < secondHalf.length; j++) {
-//       if (firstHalf[i] === secondHalf[j]) {
-//         account.push(firstHalf[i]);
-//       }
-//     }
-//   }
-//   const deduped = [...new Set(account)];
-
-//   return deduped;
-// });
-// let sum = 0;
-// const result = sacks.map((sack) =>
-//   sack.map((s) => (sum += alphabet.indexOf(s) + 1))
-// );
-
-// console.log({ sum });
-
-//Reafcatoring
 const text = await Deno.readTextFile("./input.txt");
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
